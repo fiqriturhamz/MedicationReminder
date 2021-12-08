2416b91c378a016ca4c06c6ab1dbc685811971cf
Ilyam Faisal <5618385-ilyamfaisal28@users.noreplay.gitlab.com>
2021-12-08 09:55:30 +0700

    Menambahkan activity dose detail dan color.xml

---
+++ app/src/main/java/com/example/medicationreminder/DoseDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.medicationreminder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoseDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dose_detail)
+    }
+}
